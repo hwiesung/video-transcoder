2657094f2c59851f0c8d99b37a2b322e84825d53
hwiesung <hwiesung@gmail.com>
2019-01-22 10:58:05 +0900

    transcoding to mp4

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,9 @@ var serviceAccount = require("../serviceAccountKey.json");
 var axios = require('axios');
 var fs = require('fs');
 
+var ffmpeg = require('fluent-ffmpeg');
+
+
 var app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://peeple-7d3de.firebaseio.com",
@@ -29,7 +32,35 @@ router.post('/notify/upload', function(req, res) {
     }).then((result)=>{
         const outputFilename = './temp/'+name;
         fs.writeFileSync(outputFilename, result.data);
-        res.send(JSON.stringify({ret_code:0}));
+        console.log('local saved');
+
+
+        ffmpeg.ffprobe(outputFilename, (err, metadata)=>{
+           if(err){
+               console.log(err);
+               res.send(JSON.stringify({ret_code:1, msg:'metadata load failed'}));
+           }
+           else{
+               console.log('laod metadata');
+               console.log(metadata.format);
+               var command = ffmpeg(outputFilename).on('codecData', (data)=>{
+                   console.log(data);
+               }).on('start',()=>{
+                   console.log('processing start');
+
+               }).on('end',(stdout, stderr)=>{
+                   console.log('end');
+                   res.send(JSON.stringify({ret_code:0}));
+
+               }).on('error', (err)=>{
+                   console.log("error occured:"+err.message);
+                   res.send(JSON.stringify({ret_code:1, msg:'transcoding failed'}));
+               }).output('./temp/'+name+'.mp4').run();
+           }
+
+        });
+
+
     });
 
 });

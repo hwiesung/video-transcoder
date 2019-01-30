d974751caff4f0e4c856aca5bde9683a82eb07d8
hwiesung <hwiesung@gmail.com>
2019-01-30 18:47:48 +0900

    timezone logger

--- common/logger.js
+++ common/logger.js
@@ -1,8 +1,12 @@
 const winston = require('winston');
-
+var moment = require('moment-timezone');
+const myFormat =winston.format.printf((info)=>{
+    info.timestamp = moment().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss');
+    return JSON.stringify(info);
+});
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.combine(winston.format.timestamp({format:'YYYY-MM-DD HH:mm:ss'}), winston.format.json()),
+    format: winston.format.combine(myFormat),
     transports: [
         new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
         new winston.transports.File({ filename: 'logs/combined.log' })

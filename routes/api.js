808ed82d70538279c99f3236fc7f5c71ebe581c6
hwiesung <hwiesung@gmail.com>
2019-02-07 17:54:13 +0900

    transcoding 비동기 처리

--- routes/api.js
+++ routes/api.js
@@ -123,7 +123,7 @@ router.post('/upload/image', function(req, res) {
 });
 
 
-async function transcodingJob(uid, topicKey, videoKey, name, path){
+async function transcodingJob(uid, topicKey, videoKey, name, path, thumbnailPos){
     const tempFilePath = './temp/'+name;
 
     try{
@@ -144,22 +144,20 @@ async function transcodingJob(uid, topicKey, videoKey, name, path){
             //TODO : metadata 정보에 따른 분기 처리
 
             logger.info('metadata:'+JSON.stringify(metadata.format));
-            app.database().ref('/request/video/'+uid+'/'+videoKey+'/phrase').set(TRNAS_PHRASE.LOAD_METADATA).then(()=>{}).catch((err)=>{logger.error(err)});
-
+            app.database().ref('/request/video/'+uid+'/'+videoKey+'/phrase').set(TRNAS_PHRASE.LOAD_METADATA);
 
             let key = crypto.randomBytes(32).toString('hex');
 
             const outputFileName = moment().valueOf()+'_'+key+'.mp4';
             const outputFilePath = './temp/'+outputFileName;
-            //const thumbnailName = moment().valueOf()+'_'+key+'.png';
+
             const thumbnailName = moment().valueOf()+'_'+key+'.png';
             const thumbnailPath = './temp/'+thumbnailName;
 
             const previewName = moment().valueOf()+'_'+key+'_preview.png';
             const previewPath = './temp/'+previewName;
 
-
-            const thumbnailPos = req.body.thumbnail_pos ? req.body.thumbnail_pos : Math.floor(metadata.format.duration / 2);
+            thumbnailPos = thumbnailPos ? thumbnailPos : Math.floor(metadata.format.duration / 2);
             ffmpeg(tempFilePath).on('codecData', (data)=>{
                 logger.info('format:' + data.format +', video:'+data.video +', audio:'+data.audio+' , duration:'+data.duration);
             }).on('start',()=>{
@@ -172,7 +170,7 @@ async function transcodingJob(uid, topicKey, videoKey, name, path){
                     if(err){
                         logger.error('outputFile read failed');
                         app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_READ_OUTPUT_FILE);
-                        throw 'outputFile read failed';
+                        reject(new Error( 'outputFile read failed'));
                     }
 
                     logger.info("start upload file :" + outputFileName);
@@ -185,8 +183,8 @@ async function transcodingJob(uid, topicKey, videoKey, name, path){
                     }, (err, result)=> {
                         if (err){
                             logger.error('s3 upload failed:'+outputFileName);
-                            res.send(JSON.stringify({ret_code:RET_CODE.FAIL_UPLOAD_TO_S3, msg:'s3 upload failed'}));
-                            throw 's3 upload failed';
+                            app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_UPLOAD_TO_S3);
+                            reject(new Error( 's3 upload failed'));
                         }
 
                         logger.info('File uploaded successfully at '+ result.Location);
@@ -197,7 +195,7 @@ async function transcodingJob(uid, topicKey, videoKey, name, path){
                             if(err){
                                 logger.error('outputFile read failed');
                                 app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_DOWNLOAD_FILE);
-                                throw 'outputFile read failed';
+                                reject(new Error( 'outputFile read failed'));
                             }
 
                             logger.info("start upload file :" + thumbnailName);
@@ -211,7 +209,7 @@ async function transcodingJob(uid, topicKey, videoKey, name, path){
                                 if (err){
                                     logger.error('s3 upload failed:'+thumbnailName);
                                     app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_UPLOAD_TO_S3);
-                                    throw 's3 upload failed';
+                                    reject(new Error( 's3 upload failed'));
                                 }
 
                                 logger.info('File uploaded successfully at '+ result.Location);
@@ -221,7 +219,7 @@ async function transcodingJob(uid, topicKey, videoKey, name, path){
                                     if(err){
                                         logger.error('outputFile read failed');
                                         app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_DOWNLOAD_FILE);
-                                        throw 'outputFile read failed';
+                                        reject(new Error( 'outputFile read failed'));
                                     }
 
                                     logger.info("start upload file :" + previewName);
@@ -234,8 +232,8 @@ async function transcodingJob(uid, topicKey, videoKey, name, path){
                                     }, async (err, result)=> {
                                         if (err){
                                             logger.error('s3 upload failed:'+previewName);
-                                            app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_UPLOAD_TO_S3);
-                                            throw 's3 upload failed';
+                                            await app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_UPLOAD_TO_S3);
+                                            reject(new Error( 's3 upload failed'));
                                         }
 
                                         logger.info('File uploaded successfully at '+ result.Location);
@@ -247,7 +245,6 @@ async function transcodingJob(uid, topicKey, videoKey, name, path){
                                         const streaming_url = 'http://ec2-13-125-219-151.ap-northeast-2.compute.amazonaws.com:3001/streaming/'+config.s3.bucket+'/'+outputFileName;
                                         const thumbnail_url = 'http://ec2-13-125-219-151.ap-northeast-2.compute.amazonaws.com:3001/image/'+config.s3.bucket+'/'+thumbnailName;
                                         const preview_url = 'http://ec2-13-125-219-151.ap-northeast-2.compute.amazonaws.com:3001/image/'+config.s3.bucket+'/'+previewName;
-                                        logger.info('111');
                                         const video = {
                                             create_time : now,
                                             streaming_url : streaming_url,
@@ -255,13 +252,12 @@ async function transcodingJob(uid, topicKey, videoKey, name, path){
                                             preview_url:preview_url,
                                             title : name,
                                             topic_key:topicKey,
-                                            uid : context.params.uid
+                                            uid : uid
                                         };
 
                                         updates['/video/'+videoKey] = video;
                                         updates['/request/video/'+uid+'/'+videoKey+'/complete_time'] = now;
                                         updates['/request/video/'+uid+'/'+videoKey+'/phrase'] = TRNAS_PHRASE.COMPLETE;
-                                        logger.info('222');
 
                                         const notification = {
                                             code: 'UPLOAD_COMPLETE',
@@ -271,9 +267,7 @@ async function transcodingJob(uid, topicKey, videoKey, name, path){
                                         let notiKey = app.database().ref('/notification/'+uid).push().key;
 
                                         updates['/notification/'+uid+'/'+notiKey] = notification;
-                                        logger.info('333');
                                         logger.info(JSON.stringify(updates));
-
 
                                         try{
                                             await app.database().ref().update(updates);
@@ -283,9 +277,6 @@ async function transcodingJob(uid, topicKey, videoKey, name, path){
                                         }
 
                                         resolve();
-
-                                        //res.send(JSON.stringify({ret_code:0, file_key:outputFileName, thumbnail_key:thumbnailName, preview_key:previewName, bucket:config.s3.bucket}));
-
                                     });
                                 });
 
@@ -325,10 +316,10 @@ router.post('/upload/video', (req, res) => {
         throw 'input error';
     }
 
+    res.send(JSON.stringify({ret_code:0}));
 
-
-    return transcodingJob(uid, topicKey, videoKey, name, path).then(()=>{
-        res.send(JSON.stringify({ret_code:0}));
+    return transcodingJob(uid, topicKey, videoKey, name, path, req.body.thumbnail_pos).then(()=>{
+        logger.info('done:'+videoKey);
     });
 });
 

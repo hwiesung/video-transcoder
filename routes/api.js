8125f89c2c2324a8f843e37c72d3c6943f2bfd50
hwiesung <hwiesung@gmail.com>
2019-02-08 10:12:08 +0900

    트랜스코더에서는 결과물만 넘기기

--- routes/api.js
+++ routes/api.js
@@ -123,28 +123,28 @@ router.post('/upload/image', function(req, res) {
 });
 
 
-async function transcodingJob(uid, topicKey, videoKey, name, path, thumbnailPos){
+async function transcodingJob(uid, type, key, name, path, thumbnailPos){
     const tempFilePath = './temp/'+name;
 
     try{
         await bucket.file(path).download({destination: tempFilePath});
     }catch(err){
         logger.error(err);
-        app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(constant.RET_CODE.FAIL_DOWNLOAD_FILE);
+        app.database().ref('/request/'+type+'/'+uid+'/'+key+'/result').set(constant.RET_CODE.FAIL_DOWNLOAD_FILE);
     }
 
     await new Promise( (resolve, reject)=>{
         ffmpeg.ffprobe(tempFilePath, (err, metadata)=>{
             if(err){
                 logger.error('metadata load fail');
-                app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_LOAD_METADATA);
+                app.database().ref('/request/'+type+'/'+uid+'/'+key+'/result').set(RET_CODE.FAIL_LOAD_METADATA);
                 reject(new Error('metadata load failed'));
             }
 
             //TODO : metadata 정보에 따른 분기 처리
 
             logger.info('metadata:'+JSON.stringify(metadata.format));
-            app.database().ref('/request/video/'+uid+'/'+videoKey+'/phrase').set(TRNAS_PHRASE.LOAD_METADATA);
+            app.database().ref('/request/'+type+'/'+uid+'/'+key+'/phrase').set(TRNAS_PHRASE.LOAD_METADATA);
 
             let key = crypto.randomBytes(32).toString('hex');
 
@@ -162,14 +162,14 @@ async function transcodingJob(uid, topicKey, videoKey, name, path, thumbnailPos)
                 logger.info('format:' + data.format +', video:'+data.video +', audio:'+data.audio+' , duration:'+data.duration);
             }).on('start',()=>{
                 logger.info('processing start : ' + outputFileName);
-                app.database().ref('/request/video/'+uid+'/'+videoKey+'/phrase').set(TRNAS_PHRASE.START_TRANSCODING);
+                app.database().ref('/request/'+type+'/'+uid+'/'+key+'/phrase').set(TRNAS_PHRASE.START_TRANSCODING);
             }).on('end',(stdout, stderr)=>{
                 logger.info('processing finish : '+outputFileName);
-                app.database().ref('/request/video/'+uid+'/'+videoKey+'/phrase').set(TRNAS_PHRASE.FINISH_TRANSCODING);
+                app.database().ref('/request/'+type+'/'+uid+'/'+key+'/phrase').set(TRNAS_PHRASE.FINISH_TRANSCODING);
                 fs.readFile(outputFilePath, (err, data) => {
                     if(err){
                         logger.error('outputFile read failed');
-                        app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_READ_OUTPUT_FILE);
+                        app.database().ref('/request/'+type+'/'+uid+'/'+key+'/result').set(RET_CODE.FAIL_READ_OUTPUT_FILE);
                         reject(new Error( 'outputFile read failed'));
                     }
 
@@ -183,18 +183,18 @@ async function transcodingJob(uid, topicKey, videoKey, name, path, thumbnailPos)
                     }, (err, result)=> {
                         if (err){
                             logger.error('s3 upload failed:'+outputFileName);
-                            app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_UPLOAD_TO_S3);
+                            app.database().ref('/request/'+type+'/'+uid+'/'+key+'/result').set(RET_CODE.FAIL_UPLOAD_TO_S3);
                             reject(new Error( 's3 upload failed'));
                         }
 
                         logger.info('File uploaded successfully at '+ result.Location);
                         fs.unlinkSync(outputFilePath);
                         fs.unlinkSync(tempFilePath);
-                        app.database().ref('/request/video/'+uid+'/'+videoKey+'/phrase').set(TRNAS_PHRASE.UPLOAD_VIDEO_FILE);
+                        app.database().ref('/request/'+type+'/'+uid+'/'+key+'/phrase').set(TRNAS_PHRASE.UPLOAD_VIDEO_FILE);
                         fs.readFile(thumbnailPath, (err, data) => {
                             if(err){
                                 logger.error('outputFile read failed');
-                                app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_DOWNLOAD_FILE);
+                                app.database().ref('/request/'+type+'/'+uid+'/'+key+'/result').set(RET_CODE.FAIL_DOWNLOAD_FILE);
                                 reject(new Error( 'outputFile read failed'));
                             }
 
@@ -208,17 +208,17 @@ async function transcodingJob(uid, topicKey, videoKey, name, path, thumbnailPos)
                             }, (err, result)=> {
                                 if (err){
                                     logger.error('s3 upload failed:'+thumbnailName);
-                                    app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_UPLOAD_TO_S3);
+                                    app.database().ref('/request/'+type+'/'+uid+'/'+key+'/result').set(RET_CODE.FAIL_UPLOAD_TO_S3);
                                     reject(new Error( 's3 upload failed'));
                                 }
 
                                 logger.info('File uploaded successfully at '+ result.Location);
                                 fs.unlinkSync(thumbnailPath);
-                                app.database().ref('/request/video/'+uid+'/'+videoKey+'/phrase').set(TRNAS_PHRASE.UPLOAD_THUMBNAIL_FILE);
+                                app.database().ref('/request/'+type+'/'+uid+'/'+key+'/phrase').set(TRNAS_PHRASE.UPLOAD_THUMBNAIL_FILE);
                                 fs.readFile(previewPath, (err, data) => {
                                     if(err){
                                         logger.error('outputFile read failed');
-                                        app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_DOWNLOAD_FILE);
+                                        app.database().ref('/request/'+type+'/'+uid+'/'+key+'/result').set(RET_CODE.FAIL_DOWNLOAD_FILE);
                                         reject(new Error( 'outputFile read failed'));
                                     }
 
@@ -232,41 +232,26 @@ async function transcodingJob(uid, topicKey, videoKey, name, path, thumbnailPos)
                                     }, async (err, result)=> {
                                         if (err){
                                             logger.error('s3 upload failed:'+previewName);
-                                            await app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(RET_CODE.FAIL_UPLOAD_TO_S3);
+                                            await app.database().ref('/request/'+type+'/'+uid+'/'+key+'/result').set(RET_CODE.FAIL_UPLOAD_TO_S3);
                                             reject(new Error( 's3 upload failed'));
                                         }
 
                                         logger.info('File uploaded successfully at '+ result.Location);
                                         fs.unlinkSync(previewPath);
 
-                                        let now = moment().valueOf();
                                         let updates = {};
 
                                         const streaming_url = 'http://ec2-13-125-219-151.ap-northeast-2.compute.amazonaws.com:3001/streaming/'+config.s3.bucket+'/'+outputFileName;
                                         const thumbnail_url = 'http://ec2-13-125-219-151.ap-northeast-2.compute.amazonaws.com:3001/image/'+config.s3.bucket+'/'+thumbnailName;
                                         const preview_url = 'http://ec2-13-125-219-151.ap-northeast-2.compute.amazonaws.com:3001/image/'+config.s3.bucket+'/'+previewName;
-                                        const video = {
-                                            create_time : now,
-                                            streaming_url : streaming_url,
-                                            thumbnail_url : thumbnail_url,
-                                            preview_url:preview_url,
-                                            title : name,
-                                            topic_key:topicKey,
-                                            uid : uid
-                                        };
 
-                                        updates['/video/'+videoKey] = video;
-                                        updates['/request/video/'+uid+'/'+videoKey+'/complete_time'] = now;
-                                        updates['/request/video/'+uid+'/'+videoKey+'/phrase'] = TRNAS_PHRASE.COMPLETE;
+                                        updates['/request/'+type+'/'+uid+'/'+key+'/streaming_url'] = streaming_url;
+                                        updates['/request/'+type+'/'+uid+'/'+key+'/thumbnail_url'] = thumbnail_url;
+                                        updates['/request/'+type+'/'+uid+'/'+key+'/preview_url'] = preview_url;
+                                        updates['/request/'+type+'/'+uid+'/'+key+'/complete_time'] = moment().valueOf();
+                                        updates['/request/'+type+'/'+uid+'/'+key+'/phrase'] = TRNAS_PHRASE.COMPLETE;
+                                        updates['/request/'+type+'/'+uid+'/'+key+'/result'] = RET_CODE.SUCCESS;
 
-                                        const notification = {
-                                            code: 'UPLOAD_COMPLETE',
-                                            payload: '{"KEY":"' + videoKey + '"}'
-                                        };
-
-                                        let notiKey = app.database().ref('/notification/'+uid).push().key;
-
-                                        updates['/notification/'+uid+'/'+notiKey] = notification;
                                         logger.info(JSON.stringify(updates));
 
                                         try{
@@ -288,7 +273,7 @@ async function transcodingJob(uid, topicKey, videoKey, name, path, thumbnailPos)
                 });
             }).on('error', (err)=>{
                 logger.error('transcoding failed :' +err.message);
-                app.database().ref('/request/video/'+uid+'/'+videoKey+'/result').set(constant.RET_CODE.FAIL_TRANSCODING);
+                app.database().ref('/request/video/'+uid+'/'+key+'/result').set(constant.RET_CODE.FAIL_TRANSCODING);
             }).output(outputFilePath).audioCodec('aac').videoCodec('libx264').output(thumbnailPath).outputOptions('-frames', '1').noAudio().seek(thumbnailPos).output(previewPath).outputOptions('-frames', '1').noAudio().seek(0).run();
         });
     });
@@ -299,9 +284,9 @@ async function transcodingJob(uid, topicKey, videoKey, name, path, thumbnailPos)
 router.post('/upload/video', (req, res) => {
     const path = req.body.path;
     const name = req.body.name;
+    const type = req.body.type;
+    const key = req.body.key;
     const uid = req.body.uid;
-    const videoKey = req.body.video_key;
-    const topicKey = req.body.topic_key;
 
     const secretKey = req.body.secret_key;
     logger.info('input data: '+ JSON.stringify(req.body));
@@ -311,15 +296,15 @@ router.post('/upload/video', (req, res) => {
         throw 'permission denied';
     }
 
-    if(!uid || !path || !name || !topicKey || !videoKey){
+    if(!path || !name || !key ){
         res.send(JSON.stringify({ret_code:RET_CODE.ERROR, msg:'Invalid Input'}));
         throw 'input error';
     }
 
     res.send(JSON.stringify({ret_code:0}));
 
-    return transcodingJob(uid, topicKey, videoKey, name, path, req.body.thumbnail_pos).then(()=>{
-        logger.info('done:'+videoKey);
+    return transcodingJob(uid, type, key, name, path, req.body.thumbnail_pos).then(()=>{
+        logger.info('done:'+key);
     });
 });
 
